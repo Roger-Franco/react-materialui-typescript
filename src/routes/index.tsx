f21ef94c138be8e4db753cedb66b1ff4cb86b638
Roger-Franco <rogerfrancogalt@gmail.com>
2022-03-19 15:55:31 -0400

    feat: Adicionado react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,14 @@
-export {};
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+// Mudanças na v6:
+// Redirect foi mudado para Navigate
+// Routes está no lugar de Switch
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Páina inicial</p>} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  )
+};
