c872de9061fc5022bc4b85b74559e4eb2523a938
Roger-Franco <rogerfrancogalt@gmail.com>
2022-03-21 20:25:57 -0400

    Opçoes de menu no drawer :sunglasses:

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,38 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import { useDrawerContext } from '../shared/contexts';
+import { useEffect } from 'react';
 
 // Mudanças na v6:
 // Redirect foi mudado para Navigate
 // Routes está no lugar de Switch
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen } = useDrawerContext();
+  const { toggleDrawerOpen, setdrawerOptions } = useDrawerContext();
+
+  useEffect(() => {
+    setdrawerOptions([
+      {
+        icon: 'home',
+        path: '/pagina-inicial',
+        label: 'Página Inicial',
+      },
+      {
+        icon: 'star',
+        path: '/cidades',
+        label: 'Cidades',
+      }
+    ]);
+  }, []);
 
   return (
     <Routes>
       <Route path="/pagina-inicial" element={<Button
         variant='contained' color='primary'
         onClick={toggleDrawerOpen}>toggle Drawer</Button>} />
+      <Route path="/cidades" element={<Button
+        variant='contained' color='primary'
+        onClick={toggleDrawerOpen}>Cidades</Button>} />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,12 +1,42 @@
 import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
+import { useNavigate, useResolvedPath, useMatch } from 'react-router-dom';
+
 import { useDrawerContext } from '../../contexts';
+
+interface IListItemLinkProps {
+  to: string;
+  icon: string;
+  label: string;
+  onClick: (() => void) | undefined;
+}
+
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
+  const navigate = useNavigate();
+
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.(); // onClick && onClick() || if(onClick) onClick() *Essas 3 formas são semelhantes
+  };
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  );
+};
 
 
 export const MenuLateral: React.FC = ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
-  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+  const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
 
   return (
     <>
@@ -24,12 +54,15 @@ export const MenuLateral: React.FC = ({ children }) => {
 
           <Box flex={1}>
             <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Página Inicial" />
-              </ListItemButton>
+              {drawerOptions.map(drawerOption => (
+                <ListItemLink
+                  key={drawerOption.path}
+                  icon={drawerOption.icon}
+                  to={drawerOption.path}
+                  label={drawerOption.label}
+                  onClick={smDown ? toggleDrawerOpen : undefined}
+                />
+              ))}
             </List>
 
           </Box>
